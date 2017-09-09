ba3a1adf352d241a60db59316edd3a5b3d9491ef
Bruno Rafael <bruninho_lito@hotmail.com>
2017-09-09 11:24:36 -0300

    Environment add

---
+++ src/Environment.js
@@ -0,0 +1,32 @@
+const {
+    Environment,
+    Network,
+    RecordSource,
+    Store,
+  } = require('relay-runtime')
+  
+const store = new Store(new RecordSource())
+
+const network = Network.create((operation, variables) => {
+
+return fetch('__RELAY_API_ENDPOINT__', {
+    method: 'POST',
+    headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+    query: operation.text,
+    variables,
+    }),
+}).then(response => {
+    return response.json()
+})
+})
+
+const environment = new Environment({
+    network,
+    store,
+})
+
+export default environment
