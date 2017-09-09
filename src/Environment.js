87191afa5f48664d4a66951ee382522d0a7c947a
Bruno Rafael <bruninho_lito@hotmail.com>
2017-09-09 11:33:38 -0300

    Post add

--- src/Environment.js
+++ src/Environment.js
@@ -8,25 +8,24 @@ const {
 const store = new Store(new RecordSource())
 
 const network = Network.create((operation, variables) => {
-
-return fetch('__RELAY_API_ENDPOINT__', {
+  return fetch('https://api.graph.cool/relay/v1/cj7dbbln40yi501084vchknnk', {
     method: 'POST',
     headers: {
-    'Accept': 'application/json',
-    'Content-Type': 'application/json'
-    },
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+      },
     body: JSON.stringify({
-    query: operation.text,
-    variables,
+      query: operation.text,
+      variables,
     }),
-}).then(response => {
-    return response.json()
-})
+  }).then(response => {
+      return response.json()
+  })
 })
 
 const environment = new Environment({
-    network,
-    store,
+  network,
+  store,
 })
 
 export default environment
